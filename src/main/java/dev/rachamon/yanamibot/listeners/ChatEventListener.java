5c6cb7390a35be3135fe4209e08a82099cefc8c0
king-glitch <58404132+king-glitch@users.noreply.github.com>
2022-01-29 14:12:19 +0700

    fixed private message

--- src/main/java/dev/rachamon/yanamibot/listeners/ChatEventListener.java
+++ src/main/java/dev/rachamon/yanamibot/listeners/ChatEventListener.java
@@ -9,75 +9,73 @@ import dev.rachamon.yanamibot.utils.YanamiBotUtil;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.filter.cause.Root;
 import org.spongepowered.api.event.message.MessageChannelEvent;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class ChatEventListener {
 
     private final YanamiBot plugin = YanamiBot.getInstance();
 
-    @Listener()
+    @Listener
     public void onChat(MessageChannelEvent.Chat event, @Root Player player) {
 
-        YanamiBot.getInstance().getLogger().debug("message cancelled : " + event.isMessageCancelled());
-        if (event.isMessageCancelled()) return;
-        YanamiBot.getInstance().getLogger().debug("raw message : " + event.getRawMessage().toPlain());
+        try {
 
-        if (event.getRawMessage().toPlain().charAt(0) == '/') return;
-        List<EventsConfig.ChatResponse> responses = new ArrayList<>(this.plugin.getEventsConfig().getChatResponses().values());
+            if (event.isMessageCancelled() || !event.getOriginalMessage().toPlain().matches("<\\[[A-Z]] ([A-Z])\\w+>.*"))
+                return;
+            List<EventsConfig.ChatResponse> responses = new ArrayList<>(this.plugin.getEventsConfig().getChatResponses().values());
 
-        Optional<EventsConfig.ChatResponse> firstMatch = responses.stream().filter(m -> m.getRegexes().stream().anyMatch(a -> event.getRawMessage().toPlain().toLowerCase().matches(a))).findFirst();
+            Optional<EventsConfig.ChatResponse> firstMatch = responses.stream().filter(m -> m.getRegexes().stream().anyMatch(a -> event.getRawMessage().toPlain().toLowerCase().matches(a))).findFirst();
 
-        if (!firstMatch.isPresent()) return;
+            if (!firstMatch.isPresent()) return;
 
-        boolean hasPermission = firstMatch.get().getPermission().isEmpty() || player.hasPermission(firstMatch.get().getPermission());
+            boolean hasPermission = firstMatch.get().getPermission().isEmpty() || player.hasPermission(firstMatch.get().getPermission());
 
-        if (!hasPermission) return;
+            if (!hasPermission) return;
 
-        Sponge.getScheduler().createTaskBuilder()
-                .execute(() -> {
-                    LanguageConfig language = plugin.getLanguage();
+            Sponge.getScheduler().createTaskBuilder()
+                    .execute(() -> {
+                        LanguageConfig language = plugin.getLanguage();
 
-                    Sponge.getServer().getOnlinePlayers().forEach(p -> {
-                        p.sendMessage(YanamiBotUtil.toText(
-                                language.getGeneralCategory()
-                                        .getMessageBuilder()
-                                        .replaceAll("\\{target}", player.getName())
-                                        .replaceAll("\\{bot-name}", language.getGeneralCategory().getBotName())
-                                        .replaceAll("\\{message}", firstMatch.get().getResponses().get(new Random().nextInt(firstMatch.get().getResponses().size())))
-                        ));
-                    });
+                        Sponge.getServer().getOnlinePlayers().forEach(p -> {
+                            p.sendMessage(YanamiBotUtil.toText(
+                                    language.getGeneralCategory()
+                                            .getMessageBuilder()
+                                            .replaceAll("\\{target}", player.getName())
+                                            .replaceAll("\\{bot-name}", language.getGeneralCategory().getBotName())
+                                            .replaceAll("\\{message}", firstMatch.get().getResponses().get(new Random().nextInt(firstMatch.get().getResponses().size())))
+                            ));
+                        });
 
-                    if (firstMatch.get().getCommands().size() > 0) {
+                        if (firstMatch.get().getCommands().size() > 0) {
 
-                        ChatQuestion question = ChatQuestion.of(YanamiBotUtil.toText(language.getGeneralCategory()
-                                        .getMessageBuilderRaw()
-                                        .replaceAll("\\{target}", player.getName())
-                                        .replaceAll("\\{bot-name}", this.plugin.getLanguage().getGeneralCategory().getBotName())
-                                        .replaceAll("\\{message}", language.getGeneralCategory().getRunCommandsQuestion())))
-                                .addAnswer(ChatQuestionAnswer.of(YanamiBotUtil.toText(language.getGeneralCategory().getClickAcceptButton()), target -> {
-                                    firstMatch.get().getCommands().forEach(command -> {
-                                        this.plugin.getGame().getCommandManager().process(Sponge.getServer().getConsole(), command.replaceAll("\\{player}", player.getName()));
-                                    });
-                                })).build();
+                            ChatQuestion question = ChatQuestion.of(YanamiBotUtil.toText(language.getGeneralCategory()
+                                            .getMessageBuilderRaw()
+                                            .replaceAll("\\{target}", player.getName())
+                                            .replaceAll("\\{bot-name}", this.plugin.getLanguage().getGeneralCategory().getBotName())
+                                            .replaceAll("\\{message}", language.getGeneralCategory().getRunCommandsQuestion())))
+                                    .addAnswer(ChatQuestionAnswer.of(YanamiBotUtil.toText(language.getGeneralCategory().getClickAcceptButton()), target -> {
+                                        firstMatch.get().getCommands().forEach(command -> {
+                                            this.plugin.getGame().getCommandManager().process(Sponge.getServer().getConsole(), command.replaceAll("\\{player}", player.getName()));
+                                        });
+                                    })).build();
 
-                        question.setAlreadyResponse(YanamiBotUtil.toText(language.getQuestionCategory().getAlreadyResponded()));
-                        question.setClickToAnswer(YanamiBotUtil.toText(language.getQuestionCategory().getClickToAnswer()));
-                        question.setClickToView(YanamiBotUtil.toText(language.getQuestionCategory().getClickToView()));
-                        question.setMustBePlayer(YanamiBotUtil.toText(language.getQuestionCategory().getMustBePlayer()));
-                        question.pollChat(player);
-                    }
+                            question.setAlreadyResponse(YanamiBotUtil.toText(language.getQuestionCategory().getAlreadyResponded()));
+                            question.setClickToAnswer(YanamiBotUtil.toText(language.getQuestionCategory().getClickToAnswer()));
+                            question.setClickToView(YanamiBotUtil.toText(language.getQuestionCategory().getClickToView()));
+                            question.setMustBePlayer(YanamiBotUtil.toText(language.getQuestionCategory().getMustBePlayer()));
+                            question.pollChat(player);
+                        }
 
-                })
-                .delay(500, TimeUnit.MILLISECONDS)
-                .submit(this.plugin);
+                    })
+                    .delay(500, TimeUnit.MILLISECONDS)
+                    .submit(this.plugin);
+        } catch (Exception e) {
+            this.plugin.getLogger().debug(e.getMessage());
+        }
 
     }
 
