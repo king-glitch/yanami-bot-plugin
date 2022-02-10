a2db9c9a442ceaf6d579c855c09ebc57d0875927
king-glitch <58404132+king-glitch@users.noreply.github.com>
2022-02-10 16:51:01 +0700

    forget to delete somecode

--- src/main/java/dev/rachamon/yanamibot/YanamiBotModule.java
+++ src/main/java/dev/rachamon/yanamibot/YanamiBotModule.java
@@ -2,7 +2,6 @@ package dev.rachamon.yanamibot;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
-import dev.rachamon.yanamibot.api.services.YanamiBotService;
 import dev.rachamon.yanamibot.managers.BotManager;
 
 /**
@@ -12,6 +11,5 @@ public class YanamiBotModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(BotManager.class).in(Scopes.SINGLETON);
-        bind(YanamiBotService.class).in(Scopes.SINGLETON);
     }
 }
